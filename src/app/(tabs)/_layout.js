05bb01e46e42f53825adcf33683f4932d4c74444
Ian Souza <ian64208@gmail.com>
2026-03-23 13:51:50 -0300

    add headerShown: false on tabs

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -3,18 +3,18 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'red' }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'red', headerShown: false }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "",
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
+          title: "",
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
         }}
       />
